51313390d72bcd25119437735f8c7c0a3aeab2ed
Arthur <45287989+kovert99@users.noreply.github.com>
2019-05-02 23:38:22 +0300

    Work with redux

---
+++ app/src/actions/index.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import {
+  AUTH_SIGN_UP,
+  AUTH_SIGN_OUT,
+  AUTH_SIGN_IN,
+  AUTH_ERROR
+} from './types';
+
+export const signUp = data => {
+  return async dispatch => {
+    try {
+      const res = await axios.post('http://localhost:5000/users/signup', data);
+
+      dispatch({
+        type: AUTH_SIGN_UP,
+        payload: res.data.token
+      });
+
+      localStorage.setItem('JWT_TOKEN', res.data.token);
+      axios.defaults.headers.common['Authorization'] = res.data.token;
+    } catch(err) {
+      dispatch({
+        type: AUTH_ERROR,
+        payload: 'Email is already in use'
+      })
+    }
+  };
+}
+
+export const signIn = data => {
+  return async dispatch => {
+    try {
+      const res = await axios.post('http://localhost:5000/users/signin', data);
+
+      dispatch({
+        type: AUTH_SIGN_IN,
+        payload: res.data.token
+      });
+
+      localStorage.setItem('JWT_TOKEN', res.data.token);
+      axios.defaults.headers.common['Authorization'] = res.data.token;
+    } catch(err) {
+      dispatch({
+        type: AUTH_ERROR,
+        payload: 'Email and password combination isn\'t valid'
+      })
+    }
+  };
+}
+
+export const signOut = () => {
+  return dispatch => {
+    localStorage.removeItem('JWT_TOKEN');
+    axios.defaults.headers.common['Authorization'] = '';
+
+    dispatch({
+      type: AUTH_SIGN_OUT,
+      payload: ''
+    })
+  };
+}
--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
 
 ReactDOM.render((
   <BrowserRouter>
